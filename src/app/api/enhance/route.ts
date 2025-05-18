b966e4fa495a982b046f2bcae48e5f2230cbf48e
ScorchedPearl <iit2024087@iiita.ac.in>
2025-05-19 00:37:47 +0530

    fix ai

--- src/app/api/enhance/route.ts
+++ src/app/api/enhance/route.ts
@@ -1,6 +1,6 @@
 import { HfInference } from '@huggingface/inference';
 
-const inference = new HfInference(process.env.HUGGINGFACEHUB_ACCESS_TOKEN);
+const inference = new HfInference("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");
 
 export async function POST(req:Request){
   try{
